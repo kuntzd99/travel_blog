289fe3f36e1a4c6f6c591f918cdb8b588acc9088
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-07 07:49:20 -0500

    Liking locations works

--- client/src/services/LikesService.js
+++ client/src/services/LikesService.js
@@ -3,10 +3,17 @@ import Client from '.'
 export const GetLikes = async (id) => {
   try {
     const res = await Client.get(`/likes/${id}`)
-    return res.data.likes
+    return res.data.locationLikes
   } catch (error) {
     throw error
   }
 }
 
-export const UpdateLikes = async (id) => {}
+export const UpdateLike = async (id, formValue) => {
+  try {
+    const res = await Client.put(`/likes/${id}`, formValue)
+    return res.data.like
+  } catch (error) {
+    throw error
+  }
+}
--- client/src/store/actions/LikesAction.js
+++ client/src/store/actions/LikesAction.js
@@ -1,13 +1,27 @@
-import { GetLikes } from '../../services/LikesService'
+import { GetLikes, UpdateLike } from '../../services/LikesService'
 import { GET_LIKES } from '../types'
 
-export const LoadLikes = () => {
+export const LoadLikes = (id) => {
   return async (dispatch) => {
     try {
-      const likes = await GetLikes()
+      const likes = await GetLikes(id)
       dispatch({
         type: GET_LIKES,
         payload: likes
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+export const AddLike = (id, formValue) => {
+  return async (dispatch) => {
+    try {
+      const like = await UpdateLike(id, formValue)
+      dispatch({
+        type: GET_LIKES,
+        payload: like
       })
     } catch (error) {
       throw error
